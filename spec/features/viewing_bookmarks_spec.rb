940715c34893043a0daf4f1f789f76eacad4c5b9
KKassim <k_master@hotmail.co,uk>
2022-02-25 11:45:38 +0000

    refactoring viewing  bookmarks

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,8 +1,10 @@
 require './app'
 
 feature 'Viewing bookmarks' do
-  scenario 'visiting the index page' do
-    visit('/')
-    expect(page).to have_content 'Bookmark Manager'
+  scenario 'visiting the bookmarks page' do
+    visit('/bookmarks')
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
   end
 end
