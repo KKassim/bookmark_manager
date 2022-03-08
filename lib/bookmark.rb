93b0e664fd6f6735779a42e9f1df68fba9022dde
KKassim <k_master@hotmail.co,uk>
2022-03-08 10:47:15 +0000

    made updating bookmark test

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -45,4 +45,19 @@ class Bookmark
     end
     connection.exec_params("DELETE FROM bookmarks WHERE id = $1", [id])
     end
+
+    def self.update(id:)
+        if ENV['RACK_ENV'] == 'test'
+            connection = PG.connect(dbname: 'bookmark_manager_test')
+          else
+            connection = PG.connect(dbname: 'bookmark_manager')
+          end
+            connection = PG.connect(dbname: 'bookmark_manager_test')
+  connection.exec_params(
+    "UPDATE bookmarks SET url = $1, title = $2 WHERE id = $3",
+    [ params[:url], params[:title], params[:id] ]
+  )
+
+  redirect('/bookmarks')
+    end
 end
