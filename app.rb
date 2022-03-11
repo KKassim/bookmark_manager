b8e9a55bfa9e32fe73c0aef668348fb2e6863f06
KKassim <k_master@hotmail.co,uk>
2022-03-11 16:14:21 +0000

    moving sql out of the controller

--- app.rb
+++ app.rb
@@ -22,15 +22,14 @@ class BookmarkManager < Sinatra::Base
     erb :'bookmarks/new'
   end
 
+  get '/bookmarks/:id/comments/new' do
+    @bookmark_id = params[:id]
+    erb :'comments/new'
+  end
+
   post '/bookmarks' do
     flash[:notice] = "You must submit a valid URL." unless Bookmark.create(url: params[:url], title: params[:title])
     redirect('/bookmarks')
-
-  end
-
-  delete '/bookmarks/:id' do
-    Bookmark.delete(id: params[:id])
-    redirect '/bookmarks'
   end
 
   get '/bookmarks/:id/edit' do
@@ -38,14 +37,23 @@ class BookmarkManager < Sinatra::Base
     erb :"bookmarks/edit"
   end
 
+  delete '/bookmarks/:id' do
+    Bookmark.delete(id: params[:id])
+    redirect '/bookmarks'
+  end
+
   patch '/bookmarks/:id' do
     Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
     redirect '/bookmarks'
   end
-  
-  get '/bookmarks/:id/edit' do
-    @bookmark = Bookmark.find(id: params[:id])
-    erb :'bookmarks/edit' 
+
+  post '/bookmarks/:id/comments' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec_params(
+      "INSERT INTO comments (text, bookmark_id) VALUES($1, $2);",
+      [params[:comment], params[:id]]
+      )
+    redirect '/bookmarks'
   end
 
   run! if app_file == $0
